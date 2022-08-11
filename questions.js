f625a991e07ca2183c69a747505f6ae06534ed72
cjacksongomes2 <108413263+cjacksongomes2@users.noreply.github.com>
2022-08-10 21:07:38 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,86 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which is not valid data type in Javascript?",
+    choices: ["All the below", "float", "number", "undefineded"],
+    answer: "float"
+  },
+  {
+    title: "Which type of language is Javascript?",
+    choices: [
+      "Programming",
+      "Scripting",
+      "Markup",
+      "None of the above"
+    ],
+    answer: "Scripting"
+  },
+  {
+    title: "Which tag is used to write the javascipt code:",
+    choices: ["<script>", "<sp>", "<java>", "<javascript>"],
+    answer: "<script>"
+  },
+  {
+    title: "Javascript is which type of language:",
+    choices: [
+      "Interpreted",
+      "Complied",
+      "None of the above",
+      "All the above"
+    ],
+    answer: "Interpreted"
+  },
+  {
+    title:
+      "Which one of these is not among the three different types of errors in JavaScript?",
+    choices: [
+      "Animation time errors",
+      "Load time errors",
+      "Run time errors",
+      "Logical Errors"
+    ],
+    answer: "Animation time errors"
+  },
+  {
+    title: "A function defintion expressio is known as",
+    choices: [
+      "Function calls",
+      "Function definiation",
+      "Function calling",
+      "Function literal"
+    ],
+    answer: "Function literal"
+  },
+  {
+    title: "Which one is ternary operator?",
+    choices: ["?*", "?:", ">?", "<?"],
+    answer: "?:"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "What is HTML?",
+    choices: ["Hyermark Language", "Hypermix Language", "Hypertext Markup Language", "Hypertension Language"],
+    answer: "Hypertext Markup Language"
+  },
+  {
+    title:
+      "Which tag is used to create a check box ",
+    choices: ["<checkbox>", "<Input type=checkbox> ", "for loops", "console.log"],
+    answer: "<Input type=checkbox"
+  },
+  {
+    title: "From which tag desciptive list starts?:",
+    choices: ["<LL>", "<DL>", "<LDL>", "<DD>"],
+    answer: "<DL>"
+  }
+];
